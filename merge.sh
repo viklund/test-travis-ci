7ad1f9d6035ce07c88a96d7b883cd0046d0b836b
Johan Viklund <johan.viklund@nbis.se>
2018-03-17 19:23:12 +0100

    Just push master

--- merge.sh
+++ merge.sh
@@ -15,4 +15,4 @@ git checkout master
 git merge "$TRAVIS_COMMIT" || exit
 echo "MERGED"
 PAGER=cat git log --graph --oneline --decorate --all
-git push -q https://$GITHUB_TOKEN:x-oauth-basic@github.com/NBISweden/K9-WGS-Pipeline.git HEAD:master
+git push -q https://$GITHUB_TOKEN:x-oauth-basic@github.com/NBISweden/K9-WGS-Pipeline.git master

688c607d84a2f9e5a3613c8a2ea6ec3c8698b14c
Johan Viklund <johan.viklund@nbis.se>
2018-03-17 22:28:01 +0100

    Do the check when we actually have a master branch to compare to

--- merge.sh
+++ merge.sh
@@ -6,15 +6,18 @@ if [ "$TRAVIS_BRANCH" != "develop" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
     exit 0;
 fi
 
-if ! git diff --quiet origin/master -- merge.sh; then
-    echo "Diff introduces changes to merge script, won't automerge"
-    exit 1;
-fi
-
 git config user.email "johan.viklund@nbis.se"
 git config user.name "Travis CI"
 
 git fetch --depth=50 origin refs/heads/master:master
 git checkout master
+
+# Some suspenders to help us detect possible mallice
+if ! git diff --quiet master -- merge.sh; then
+    echo "Diff introduces changes to merge script, won't automerge"
+    exit 1;
+fi
+
+
 git merge --no-ff "$TRAVIS_COMMIT"
 git push -q https://$GITHUB_TOKEN:x-oauth-basic@github.com/viklund/test-travis-ci.git master:refs/heads/master

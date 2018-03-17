0993bae977be71db511b6e6674c418be5361ab3b
Johan Viklund <johan.viklund@nbis.se>
2018-03-17 18:51:22 +0100

    Fetch origin/master in merge script

--- merge.sh
+++ merge.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "MERGE BRANCH"
+
 # Only auto merge develop branch for now
 if [ "$TRAVIS_BRANCH" != "develop" ]; then
     exit 0;
@@ -8,6 +10,7 @@ fi
 export GIT_COMMITER_EMAIL='johan.viklund@nbis.se'
 export GIT_COMMITER_NAME='Johan Viklund'
 
+git fetch --depth=50 origin refs/heads/master:master
 git merge origin/master || exit
 echo "MERGED"
 #git push -q https://$GITHUB_TOKEN:x-oauth-basic@github.com/NBISweden/K9-WGS-Pipeline.git HEAD:master

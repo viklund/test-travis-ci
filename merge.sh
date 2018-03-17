449a1f443044098b05fd06fc5b30242010aba72c
Johan Viklund <johan.viklund@nbis.se>
2018-03-17 22:10:23 +0100

    Try to get author correct

--- merge.sh
+++ merge.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
+set -e
 
-# Only auto merge develop branch for now
-if [ "$TRAVIS_BRANCH" != "develop" ]; then
+# Only auto merge develop branch and non-PRs
+if [ "$TRAVIS_BRANCH" != "develop" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
     exit 0;
 fi
 
-export GIT_COMMITER_EMAIL='johan.viklund@nbis.se'
-export GIT_COMMITER_NAME='Travis CI'
-
-env
+git config user.email "johan.viklund@nbis.se"
+git config user.name "Travis CI"
 
 git fetch --depth=50 origin refs/heads/master:master
 git checkout master
-git merge --no-ff "$TRAVIS_COMMIT" || exit
+git merge --no-ff "$TRAVIS_COMMIT"
 git push -q https://$GITHUB_TOKEN:x-oauth-basic@github.com/viklund/test-travis-ci.git master:refs/heads/master

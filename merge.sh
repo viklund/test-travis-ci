9e602bf3429ac596d49106865fd7822b11fa713d
Johan Viklund <johan.viklund@nbis.se>
2018-03-17 18:54:41 +0100

    Checkout master first

--- merge.sh
+++ merge.sh
@@ -11,6 +11,7 @@ export GIT_COMMITER_EMAIL='johan.viklund@nbis.se'
 export GIT_COMMITER_NAME='Johan Viklund'
 
 git fetch --depth=50 origin refs/heads/master:master
-git merge origin/master || exit
+git checkout master
+git merge "$TRAVIS_COMMIT" || exit
 echo "MERGED"
 #git push -q https://$GITHUB_TOKEN:x-oauth-basic@github.com/NBISweden/K9-WGS-Pipeline.git HEAD:master
